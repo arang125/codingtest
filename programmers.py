c6e9c02309bf75732836c823a01208fe403203aa
arang <liebe_125@naver.com>
2023-09-25 17:23:45 +0900

    p2

--- programmers.py
+++ programmers.py
@@ -46,4 +46,45 @@ def solution(n):
     
     return answer
 
+# 자릿수 더하기
+def solution(n):
+    answer = 0
+
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    n = str(n)
+    
+    for i in range(len(n)):
+        answer += int(n[i])
+
+    return answer
+
+# 자연수 뒤집어 배열로 만들기
+def solution(n):
+    answer = []
+    n = str(n)
+    
+    for i in range(-1,-len(n)-1,-1):
+        answer.append(int(n[i]))
+    
+    return answer
+
+# 문자열을 정수로 바꾸기
+def solution(s):
+    answer = 0
+    answer = int(s)
+    return answer
+
+# 문자열 내 p와 y의 개수
+def solution(s):
+    answer = True
+    ans_dict = {'p':0, 'y':0}
+    
+    for i in s:
+        if (i == 'p') | (i == 'P') : ans_dict['p'] += 1
+        elif (i == 'y') | (i == 'Y') : ans_dict['y'] += 1
+    
+    if ans_dict['p'] != ans_dict['y'] : answer = False
+    
+    return answer
+
 # 
